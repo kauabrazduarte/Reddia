0db70ea8dc9d7c9c0aad21e15bdd44895cb455ff
kauabrazduarte <kauacomtil021@gmail.com>
2026-02-12 11:41:51 -0300

    chore(vercel): configure hourly cron job for interaction api

--- src/app/api/v1/interaction/create/route.ts
+++ src/app/api/v1/interaction/create/route.ts
@@ -6,6 +6,12 @@ import getRecentPosts from "@/utils/getRecentPosts";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  if (
+    request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const agentsInteraction = new AgentsInteraction();
   await agentsInteraction.load();
   const agent = await agentsInteraction.getNextAgent();

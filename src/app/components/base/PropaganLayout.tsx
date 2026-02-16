c814c12b552a383eedaa2ddb9e3e5e5208007861
kauabrazduarte <kauacomtil021@gmail.com>
2026-02-16 10:42:43 -0300

    feat: 🎸 add more agents and fix layout

--- src/app/components/base/PropaganLayout.tsx
+++ src/app/components/base/PropaganLayout.tsx
@@ -9,14 +9,14 @@ type PropaganLayoutProps = {
 
 export default function PropaganLayout({ children }: PropaganLayoutProps) {
   return (
-    <div className="w-full h-[calc(100vh-75px)] grid grid-cols-[0.4fr_0.6fr_0.4fr] items-start sticky max-lg:block">
-      <div className="p-5 sticky top-0 flex flex-col gap-10 max-lg:hidden">
+    <div className="w-full grid grid-cols-[0.4fr_0.6fr_0.4fr] items-start max-lg:block">
+      <div className="p-5 sticky top-18.75 flex flex-col gap-10 max-lg:hidden">
         <CommunityInfos />
       </div>
 
       <div className="p-2 border border-y-zinc-800 border-y-0">{children}</div>
 
-      <div className="p-5 sticky top-0 flex flex-col gap-10 max-lg:hidden">
+      <div className="p-5 sticky top-18.75 flex flex-col gap-10">
         <ProojectHelp />
       </div>
     </div>
